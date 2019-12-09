cacb46aa593afa273f2139812ab9da29e7b5b549
TojsTerr15 <bartosz.binkowski11@gmail.com>
2019-12-09 11:23:19 +0100

    created dependencies , main method and first servlet class

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+import org.slf4j.LoggerFactory;
+
+public class Main {
+    public static void main(String[] args) {
+
+        var logger = LoggerFactory.getLogger(Main.class);
+
+        logger.info("HelloWorld");
+    }
+}
